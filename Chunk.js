72223754752ad70e2499d3374ac4bb9972db9059
anonymous14386 <ndiramio@gmail.com>
2017-07-08 22:32:18 -0400

    Update Chunk.js

--- Chunk.js
+++ Chunk.js
@@ -92,6 +92,6 @@ var CHUNK = {
     var context = canvas.getContext('2d');
     context.font = '20pt Calibri';
     context.fillStyle = 'yellow';
-    context.fillText(message, 275, 100);
+    context.fillText(message, 0, 0);
   }
 }
