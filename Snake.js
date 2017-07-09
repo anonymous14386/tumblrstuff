494ce8d5d8da6f23d974a70ee654ab4443bde48d
anonymous14386 <ndiramio@gmail.com>
2017-07-08 22:53:19 -0400

    Update Snake.js

--- Snake.js
+++ Snake.js
@@ -32,7 +32,6 @@ var advanceGame = function() {
   if (CHUNK.detectCollisionBetween(snake, CHUNK.gameBoundaries())) {
     CHUNK.flashMessage("Whoops! you hit a wall!");
     CHUNK.endGame();
-    alert("It works");
   }
   drawSnake(snake);
 }
